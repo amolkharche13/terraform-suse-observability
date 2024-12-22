767fdd040a6c9eac7478da7cd09a0e277b9b28ae
Amol Kharche <amol.kharche@suse.com>
2024-12-22 13:50:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,17 @@
+variable "license" {
+  description = " License from SSC portal"
+  type        = string
+}
+
+variable "baseurl" {
+  description = "The external URL for SUSE Observability"
+  type        = string
+  default     = "http://localhost:8080"
+}
+
+variable "sizing" {
+  description = "Sizing profile "
+  type        = string
+  default     = "10-nonha"
+}
 
