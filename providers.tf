d28c296d80cb1e0b98baafbf4540eb08267bbe55
Amol Kharche <amol.kharche@suse.com>
2024-12-22 13:50:13 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,17 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config" # Path to your kubeconfig file
+  }
+}
 
+provider "kubernetes" {
+  config_path = "~/.kube/config"  # Path to your kubeconfig file
+}
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.12.1"
+    }
+  }
+}
