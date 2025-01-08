69c552aca96f78c722039673b037f3b4eccf7510
Amol Kharche <amol.kharche@suse.com>
2025-01-08 12:09:30 +0530

    Update main.tf

--- gcp-gke/main.tf
+++ gcp-gke/main.tf
@@ -2,20 +2,16 @@ resource "google_container_cluster" "gke_cluster" {
   name     = var.cluster_name
   location = var.region
   initial_node_count = 2
-
-  node_config {
-    machine_type = "e2-medium"
-  }
 }
 resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "my-node-pool"
+  name       = var.node_pool
   location   = var.region
   cluster    = google_container_cluster.gke_cluster.name
   node_count = 2
 
   node_config {
     preemptible  = true
-    machine_type = "e2-medium"
+    machine_type = var.machine_type
   }
 }
   management {
